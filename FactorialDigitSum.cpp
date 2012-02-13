c4bc5b57ffbcdccaf6cbe324492513a982edc6e9
nukeforum <codemunkie@gmail.com>
2012-02-13 11:36:44 -0700

    trimmed whitespace, removed redundant assignment, removed debug msgs

--- FactorialDigitSum.cpp
+++ FactorialDigitSum.cpp
@@ -2,52 +2,26 @@
 
 using namespace std;
 
-static unsigned long long factorial( unsigned long long n )
-{
+static unsigned long long factorial( unsigned long long n ) {
 	unsigned long long temp = n;
-	for( unsigned long long x = n-1; x > 0; --x )
-	{
-		temp *= x;
-	}
-
-	//cout << "factorial result: " << temp << endl;
-
+	for( unsigned long long x = n-1; x > 0; --x ){ temp *= x; }
 	return temp;
 }
 
-static unsigned long long Rn( unsigned long long n )
-{
+static unsigned long long Rn( unsigned long long n ) {
 	int digits = 0;
-	unsigned long long operatingNum = n;
-
-	operatingNum = factorial( operatingNum );
-
-	cout << "Post-factorial operatingNum: " << operatingNum << endl;
-
-	for( int x = 0; operatingNum > 0; x++ )
-	{
-		//cout << "operatingNum: " << operatingNum << endl << "mod result: " << operatingNum%10 << endl << endl;
+	unsigned long long operatingNum = factorial( n );
+	for( int x = 0; operatingNum > 0; x++ )	{
 		digits += operatingNum % 10;
 		operatingNum /= 10;
 	}
-
 	return digits;
 }
 
-int main()
-{
+int main() {
 	unsigned long long n = 1;
-
-	while( n != 8001 )
-	{
-		cout << "**********Iteration " << n << "**********" << endl;
-		cout << "result: " << Rn( n ) << endl;
-
-		if( n % 10 == 0 )
-			cin.get();
-
-		n++;
+	for( n; n != 0; n++ )	{
+		cout << "**********Iteration " << n << "**********" << endl << "result: " << Rn( n ) << endl;
 	}
-
 	return 0;
 }
